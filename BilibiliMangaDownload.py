3da1d915d2912927e350b4a065aac25f60ce0c94
LaMP57 <i@lamp.moe>
2020-05-08 23:02:15 +0800

    全新版本

--- BilibiliMangaDownload.py
+++ BilibiliMangaDownload.py
@@ -1,136 +1,98 @@
-from selenium import webdriver
-from PIL import Image
-from time import sleep
-import os
+import os, sys, getopt, zipfile, json, requests
+from io import BytesIO
 
-def get_cookies():
-    print('请在稍后打开的浏览器窗口中完成登录')
-    fireFoxOptions = webdriver.FirefoxOptions()
-    driver = webdriver.Firefox(options=fireFoxOptions)
-    driver.get('https://manga.bilibili.com')
+URL_DETAIL = "https://manga.bilibili.com/twirp/comic.v2.Comic/ComicDetail?device=pc&platform=web"
+URL_IMAGE_INDEX = "https://manga.bilibili.com/twirp/comic.v1.Comic/GetImageIndex?device=pc&platform=web"
+URL_MANGA_HOST = "https://manga.hdslb.com"
+URL_IMAGE_TOKEN = "https://manga.bilibili.com/twirp/comic.v1.Comic/ImageToken?device=pc&platform=web"
 
-    sleep(10)
-    actionButton = driver.find_element_by_class_name('action-button')
-    while actionButton.text != '历史':
-        sleep(5)
-        actionButton = driver.find_element_by_class_name('action-button')
+def downloadImage(url, path):
+    r = requests.get(url, stream=True, cookies = cookies)
+    f = open(path, 'wb')
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            f.write(chunk)
+    f.close()
 
-    cookies = driver.get_cookies()
-    print('登陆成功！')
+def getMangaInfo(mcNum):
+    data = requests.post(URL_DETAIL, data={'comic_id': mcNum}).json()['data']
+    data['ep_list'].reverse()
+    return data['title'], data['ep_list']
 
-    driver.quit()
-    return cookies
+def getImages(mcNum, chNum):
+    data = requests.post(URL_IMAGE_INDEX, data = {'ep_id': chNum}, cookies = cookies).json()['data']
+    data = bytearray(requests.get(data['host'] + data['path']).content[9:])
+    key = [chNum&0xff, chNum>>8&0xff, chNum>>16&0xff, chNum>>24&0xff, mcNum&0xff, mcNum>>8&0xff, mcNum>>16&0xff, mcNum>>24&0xff]
+    for i in range(len(data)):
+        data[i]^=key[i%8]
+    file = BytesIO(data)
+    zf = zipfile.ZipFile(file)
+    data = json.loads(zf.read('index.dat'))
+    zf.close()
+    file.close()
+    return data['pics']
 
-def optimize_webp(mangaTitle, chPath):
-    pics = os.listdir('Download/%s/%s' % (mangaTitle, chPath))
-    for pic in pics:
-        picPath = 'Download/%s/%s/%s' % (mangaTitle, chPath, pic)
-        im = Image.open(picPath)
-        im.save(picPath[:-4]+'.webp', 'WEBP')
-        os.remove(picPath)
+def getToken(url):
+    data = requests.post(URL_IMAGE_TOKEN, data = {"urls": "[\""+url+"\"]"}, cookies = cookies).json()["data"][0]
+    return '%s?token=%s' % (data["url"], data["token"])
 
-def download_manga(mcNum, chNum, isFull, arrowDirection, needLogin):
-    print('正在启动...')
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.headless = True
-    browser = webdriver.Firefox(options=fireFoxOptions)
+def getChName(chList, chNum):
+    for i in chList:
+        if i['id'] == chNum:
+            return i['short_title'] + i['title']
+    return None
 
-    if needLogin:
-        cookies = get_cookies()
-        browser.get('https://manga.bilibili.com')
-        for cookie in cookies:
-            browser.add_cookie(cookie)
-
-    url = 'https://manga.bilibili.com/mc%s/%s' % (mcNum, chNum)
-    browser.get(url)
-    browser.set_window_size(3840, 1080)
-    while browser.title == '漫画全集在线观看 - 哔哩哔哩漫画':
-        sleep(1)
-    sleep(3)
-    chTitle = browser.title
-    pageTitle = chTitle
-    chPath = pageTitle.split(' - ')[0]
-    mangaTitle = browser.title.split(' - ')[1]
-    if not(os.path.exists('Download/%s' % mangaTitle)):
-        os.mkdir('Download/%s' % mangaTitle)
-
-    i = 1
-    print('%s - %s 开始下载...' % (mangaTitle, chPath))
-    while pageTitle == chTitle:
-        chPath = pageTitle.split(' - ')[0]
-        if not(os.path.exists('Download/%s/%s' % (mangaTitle, chPath))):
-            os.mkdir('Download/%s/%s' % (mangaTitle, chPath))
-
-        pics = browser.find_elements_by_tag_name('canvas')
-
-        if len(pics) == 3:
-            pic1 = pics[0]
-            pic1.screenshot('Download/%s/%s/%s.png' % (mangaTitle, chPath, str(i*2)))
-            pic2 = pics[1]
-            pic2.screenshot('Download/%s/%s/%s.png' % (mangaTitle, chPath, str(i*2-1)))
-
-        if len(pics) == 2:
-            pic1 = pics[0]
-            pic1.screenshot('Download/%s/%s/%s.png' % (mangaTitle, chPath, str(i*2-1)))
-
-        nextBottom = browser.find_element_by_class_name(arrowDirection)
-        nextBottom.click()
-        i += 1
-
-        try:
-            browser.find_element_by_class_name('toast-content')
-        except:
-            pass
-        else:
-            chTitle = 'END'
-
-        while browser.title == '漫画全集在线观看 - 哔哩哔哩漫画':
-            sleep(1)
-        sleep(3)
-
-        pageTitle = browser.title
-        if chTitle != pageTitle:
-            print('%s - %s 下载完成！开始压缩...' % (mangaTitle, chPath))
-            optimize_webp(mangaTitle, chPath)
-            print('压缩完成！')
-
-            if isFull:
-                i = 1
-                chTitle = pageTitle
-                chPath = pageTitle.split(' - ')[0]
-                print('%s - %s 开始下载...' % (mangaTitle, chPath))
-
-    browser.quit()
+def downloadCh(mcNum, chNum, chName):
+    if not(os.path.exists('Download/%s/%s' % (mangaTitle, chName))):
+        os.mkdir('Download/%s/%s' % (mangaTitle, chName))
+    print('[INFO]', chName, '开始下载')
+    imagesURL = getImages(mcNum, chNum)
+    for idx, url in enumerate(imagesURL, 1):
+        fullURL = getToken(url)
+        path = 'Download/%s/%s/%s.jpg' % (mangaTitle, chName, str(idx))
+        downloadImage(fullURL, path)
+    print('[INFO]', chName, '下载完成')
 
 if __name__ == "__main__":
     if not(os.path.exists('Download')):
         os.mkdir('Download')
-    print('请输入mc号：(漫画详情页url中mc后的数字)')
-    mcNum = input()
 
-    print('1.日漫模式(自右向左翻页)\n2.普通模式(自左向右翻页)')
-    arrowDirection = input()
-    if arrowDirection == '1':
-        arrowDirection = 'arrow-left'
-    else:
-        arrowDirection = 'arrow-right'
+    print('请输入mc号：')
+    mcNum = int(input())
+    mangaTitle, chList = getMangaInfo(mcNum)
+    print('[INFO]', mangaTitle)
+
+    if not(os.path.exists('Download/%s' % mangaTitle)):
+        os.mkdir('Download/%s' % mangaTitle)
 
     print('1.下载单章\n2.下载全本')
     isFull = input()
     if isFull == '1':
         isFull = False
-        print('请输入要下载的章节号：(章节页url中在mc号后面的一组数字)')
-        chNum = input()
+        print('请输入要下载的章节号：')
+        chNum = int(input())
     else:
         isFull = True
-        print('请输入第一章的章节号：(章节页url中在mc号后面的一组数字)')
-        chNum = input()
 
-    print('1.直接下载\n2.需要登录')
-    needLogin = input()
-    if needLogin == '1':
-        needLogin = False
+    print('1.均为免费章节\n2.包含付费章节')
+    needCookies = input()
+    if needCookies == '1':
+        needCookies = False
     else:
-        needLogin = True
-    
-    download_manga(mcNum, chNum, isFull, arrowDirection, needLogin)
+        needCookies = True
+
+    cookies = {}
+    if needCookies:
+        print('请按说明粘贴cookies：')
+        cookiesStr = input()
+        for line in cookiesStr.split(';'):
+            key, value = line.strip().split('=', 1)
+            cookies[key] = value
+
+    if isFull:
+        for ch in chList:
+            chName = ch['short_title'] + ch['title']
+            downloadCh(mcNum, ch['id'], chName)
+    else:
+        chName = getChName(chList, chNum)
+        downloadCh(mcNum, chNum, chName)
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # BilibiliMangaDownload
 bilibili漫画下载 下载手机端提示受限无法下载的漫画 付费内容可在购买登录后下载
-请先安装firefox以及geckodriver
+
 运行后根据提示操作 例如漫画章节页url https://manga.bilibili.com/mc26742/334263 中
 mc号为26742 章节号为334263
+
+如要下载的内容中包含付费章节，则需输入cookies，请按以下方式获取cookies
+1.在浏览器中登录biliibli漫画，并购买好相应的章节
+2.在biliibli漫画主页按F12，切换到network选项卡，刷新页面
+3.选择第一行（manga.bilibili.com），在右侧找到cookies这一项，复制其中全部内容
